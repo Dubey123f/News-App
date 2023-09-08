10c23846645d8a804817ca88d3988f269a202534
Ayush Dubey <130337728+Dubey123f@users.noreply.github.com>
2023-09-08 09:53:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/Navbar';
+import News from './components/News';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News pageSize={5} country="in"/>
+      </div>
+    )
+  }
+}
